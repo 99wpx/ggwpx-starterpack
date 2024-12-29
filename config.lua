4a29259a3d27bd485edea3076063ac0c242513f2
GGWPx <shandy.sandoro.55.ss@gmail.com>
2024-12-30 07:21:57 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 Config = {}
+
 --=========================================================
 --  _______   ______ ____    __    ____  ______   ___   ___ 
 -- /  _____| /  ____|\   \  /  \  /   / |   _  \  \  \ /  / 
@@ -7,51 +8,45 @@ Config = {}
 --|  |__| | |  |__| |   \    /\    /    |  |       /  .  \  
 -- \______|  \______|    \__/  \__/     |__|      /__/ \__\ 
 --=========================================================
---      JOIN DISCORD : discord.gg/fsyxppyr8W6
+--        JOIN DISCORD : discord.gg/fsyxppyr8W6
 --=========================================================
 
--- Coordinates for the ped used to claim the starter pack
-Config.PedCoords = {
-    vector4(-1036.28, -2734.32, 20.17, 162.71),
-    vector4(-1033.3270, -2739.6233, 20.1693, 62.6877),
-    vector4(-1042.2426, -2734.9429, 20.1693, 245.4828),
-}
-
+-- Pedestrian Configuration (NPC for Starter Pack Claim)
+Config.PedCoords = vector4(-1036.28, -2734.32, 20.17, 162.71) -- Coordinates for NPC spawn
 Config.PedModel = 'u_m_m_jewelsec_01' -- Ped model
 
-Config.ClaimCommand = "claimstarter" -- Command
+-- Target System Configuration
+Config.TargetSystem = 'interact'  -- 'qb-target', 'ox-target', 'interact'
 
--- Coordinates for the vehicle to be spawned
+-- Inventory System Configuration
+Config.InventorySystem = 'qb-inventory'  -- 'qb-inventory', 'ox_inventory'
+
+-- Vehicle Spawn Configuration
 Config.VehicleSpawnCoords = {
-    vector4(-1040.2572, -2726.4758, 19.6629, 240.6792),  -- Spawn 1
-    vector4(-1034.8708, -2729.5725, 19.6641, 241.0679),  -- Spawn 2
-    vector4(-1029.3840, -2732.6367, 19.6681, 240.2436),  -- Spawn 3
+    vector4(-1040.2572, -2726.4758, 19.6629, 240.6792),  -- Spawn Point 1
+    vector4(-1034.8708, -2729.5725, 19.6641, 241.0679),  -- Spawn Point 2
+    vector4(-1029.3840, -2732.6367, 19.6681, 240.2436),  -- Spawn Point 3
 }
 
-Config.VehicleModel = 'adder' -- Vehicle model
-Config.FuelSystem = "cdn-fuel" -- "legacyfuel", "cdn-fuel"
-Config.DefaultFuelLevel = 50 -- Level Fuel (0-100)
-Config.SpawnWithVehicle = true  -- Set this to false if you don't want the player to spawn inside the vehicle
+Config.VehicleModel = 'adder'  -- Vehicle model (e.g., 'adder')
+Config.SpawnWithVehicle = true  -- Set to false to not spawn player in vehicle
+Config.FuelSystem = "cdn-fuel"  -- Fuel system options: "legacyfuel", "cdn-fuel"
+Config.DefaultFuelLevel = 50    -- Default fuel level (0-100)
 
--- Items given to the player
+-- Starter Pack Items (Items given to the player)
 Config.StarterItems = {
-    { item = 'water_bottle', count = 10 },
-    { item = 'sandwich', count = 10 },
-    { item = 'phone', count = 1 },
+    { item = 'water_bottle', count = 10 }, 
+    { item = 'sandwich', count = 10 },      
+    { item = 'phone', count = 1 },        
 }
 
--- Amount of money given
-Config.CashAmount = 1000 -- Cash amount
-Config.BankAmount = 10000 -- Bank amount
+-- Monetary Configuration (Starting funds)
+Config.CashAmount = 1000   -- Amount of cash given
+Config.BankAmount = 10000  -- Amount of money in the bank
 
--- Discord logging configuration
+-- Discord Webhook for Logging
 Config.DiscordWebhook = 'https://discord.com/api/webhooks/1284349336179703828/MjHRcV_tlc5QnxK6CwxZYxzAcUg5antf6fuuINDr-2a9qNngYzCSyleXpmnuDdoqqmd5'
 
--- Database table
+-- Database Table for Starter Pack Claims
 Config.ClaimTable = 'player_starterpacks' 
 
--- Target System
-Config.TargetSystem = 'interact' -- Options: 'qb-target', 'ox-target', 'interact'
-
--- Inventory System
-Config.InventorySystem = 'qb-inventory' -- Options: 'qb-inventory', 'ox_inventory'
